27084de15e04fd122d131cf5cc5b60c145b44320
Jomiel Enriquez <77730490+jomielenriquez@users.noreply.github.com>
2022-12-10 07:45:04 +0000

    Testing 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React from 'react';
+export default class App extends React.Component {
+  constructor(props) {
+	super(props);
+	this.state = { feedback: '', name: 'Name', email: 'email@example.com' };
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  render() {
+	return (
+  	<form className="test-mailing">
+    	<h1>Let's see if it works</h1>
+    	<div>
+      	<textarea
+        	id="test-mailing"
+        	name="test-mailing"
+        	onChange={this.handleChange}
+        	placeholder="Post some lorem ipsum here"
+        	required
+        	value={this.state.feedback}
+        	style={{width: '100%', height: '150px'}}
+      	/>
+    	</div>
+    	<input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
+  	</form>
+	)
+  }
+  handleChange(event) {
+    this.setState({feedback: event.target.value})
+  }
+  handleSubmit (event) {
+    emailjs.send("service_px08qyj","template_iyysxxo");
+    // const templateId = 'template_iyysxxo';
+    // this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email})
+    // }
+    // sendFeedback (templateId, variables) {
+    // window.emailjs.send(
+    //   'gmail', templateId,
+    //   variables
+    //   ).then(res => {
+    //     console.log('Email successfully sent!')
+    //   })
+    //   // Handle errors here however you like, or use a React error boundary
+    //   .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+    }
 }
